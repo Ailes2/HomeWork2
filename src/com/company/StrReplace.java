222a758e2bb04a9b047d2f122e8d3c62292cdd35
Ailes2 <fb.smirnov.u@gmail.com>
2021-08-02 10:41:43 +0300

    Replace

---
+++ src/com/company/StrReplace.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class StrReplace {
+    public static void main(String[] args) {
+        String s = "Qwerty12345";
+
+
+        System.out.println(replace(s));
+    }
+
+    public static String replace(String s){
+        String newstring = s.replace("123", "");
+        return newstring;
+
+    }
+
+
+
+}
